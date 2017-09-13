c749d913b11aac44a38b88f4b5cc6f545bc7ed16
Jhony Avella <jhonyavella90@gmail.com>
2017-09-13 12:02:13 -0500

    Removing problematic decorator

--- rest_framework_tm/authtoken/models.py
+++ rest_framework_tm/authtoken/models.py
@@ -3,7 +3,6 @@ import os
 
 from django.conf import settings
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 # Prior to Django 1.5, the AUTH_USER_MODEL setting does not exist.
 # Note that we don't perform this code in the compat module due to
@@ -12,7 +11,6 @@ from django.utils.encoding import python_2_unicode_compatible
 AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 
-@python_2_unicode_compatible
 class Token(models.Model):
     """
     The default authorization token model.
@@ -37,5 +35,5 @@ class Token(models.Model):
     def generate_key(self):
         return binascii.hexlify(os.urandom(20)).decode()
 
-    def __str__(self):
+    def __unicode__(self):
         return self.key
